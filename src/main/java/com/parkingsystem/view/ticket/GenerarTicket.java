9023bc76e30a6bd96640f61e6ea526dba7f3faa3
DelioCoder <davideveriwhere@gmail.com>
2024-12-05 00:23:02 -0500

    Ticket view refactored

--- src/main/java/com/parkingsystem/view/ticket/GenerarTicket.java
+++ src/main/java/com/parkingsystem/view/ticket/GenerarTicket.java
@@ -29,6 +29,11 @@ public class GenerarTicket extends javax.swing.JFrame {
         int h = this.getSize().height;
         int x = (dim.width-w)/2;
         int y = (dim.height-h)/2;
+        
+        JFecha_Entrada.setText(txtEntrada.getText());
+        JHora_Entrada.setText(txtHoraEntrada.getText());
+        JPlaca_Vehiculo.setText(BxPlacaVeh.getSelectedItem().toString());
+        JUbicacion.setText(BxUbi.getSelectedItem().toString());
     }
 
     private void inicializarControlador() {
@@ -56,13 +61,20 @@ public class GenerarTicket extends javax.swing.JFrame {
         btnCancelar = new javax.swing.JButton();
         btnGrabar = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
-        jLabel12 = new javax.swing.JLabel();
         jLabel17 = new javax.swing.JLabel();
-        jLabel18 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        JUbicacion = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
         jLabel15 = new javax.swing.JLabel();
-        jLabel16 = new javax.swing.JLabel();
+        JFecha_Entrada = new javax.swing.JLabel();
+        JHora_Entrada = new javax.swing.JLabel();
+        JPlaca_Vehiculo = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
@@ -80,16 +92,31 @@ public class GenerarTicket extends javax.swing.JFrame {
         jLabel3.setText("Fecha de entrada");
 
         txtEntrada.setText("03/10/2024");
+        txtEntrada.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtEntradaKeyReleased(evt);
+            }
+        });
 
         jLabel4.setText("Hora de entrada");
 
         txtHoraEntrada.setText("16:45");
+        txtHoraEntrada.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtHoraEntradaKeyReleased(evt);
+            }
+        });
 
         jLabel19.setText("Placa de vehículo");
 
         jLabel20.setText("Ubicación");
 
         BxPlacaVeh.setModel(new javax.swing.DefaultComboBoxModel<>());
+        BxPlacaVeh.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                BxPlacaVehItemStateChanged(evt);
+            }
+        });
         BxPlacaVeh.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BxPlacaVehActionPerformed(evt);
@@ -97,6 +124,11 @@ public class GenerarTicket extends javax.swing.JFrame {
         });
 
         BxUbi.setModel(new javax.swing.DefaultComboBoxModel<>());
+        BxUbi.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                BxUbiItemStateChanged(evt);
+            }
+        });
         BxUbi.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BxUbiActionPerformed(evt);
@@ -178,33 +210,72 @@ public class GenerarTicket extends javax.swing.JFrame {
         jPanel3.setBackground(new java.awt.Color(244, 244, 244));
         jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel12.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
-        jLabel12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/ticket-header.png"))); // NOI18N
-        jPanel3.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 80, -1, -1));
-
         jLabel17.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
         jLabel17.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/code-bar.png"))); // NOI18N
         jPanel3.add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 610, -1, -1));
 
-        jLabel18.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
-        jLabel18.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/form-ticket.png"))); // NOI18N
-        jPanel3.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 240, -1, -1));
+        jLabel16.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
+        jLabel16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/ticket-header.png"))); // NOI18N
+        jPanel3.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 70, -1, -1));
+
+        jLabel2.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel2.setText("Ubicación:");
+        jPanel3.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 410, -1, -1));
+
+        jLabel5.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel5.setText("Datos Adicionales:");
+        jPanel3.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 350, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel6.setText("Datos de Entrada:");
+        jPanel3.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 250, -1, -1));
+
+        JUbicacion.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        JUbicacion.setForeground(new java.awt.Color(153, 153, 153));
+        JUbicacion.setText("Data 4");
+        jPanel3.add(JUbicacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 410, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel8.setText("Datos de Entrada:");
+        jPanel3.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 250, -1, -1));
+
+        jLabel12.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel12.setText("Hora de entrada:");
+        jPanel3.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 320, -1, -1));
+
+        jLabel13.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel13.setText("Placa de vehiculo:");
+        jPanel3.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 380, -1, -1));
+
+        jLabel15.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        jLabel15.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel15.setText("Fecha de entrada:");
+        jPanel3.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 290, -1, -1));
+
+        JFecha_Entrada.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        JFecha_Entrada.setForeground(new java.awt.Color(153, 153, 153));
+        JFecha_Entrada.setText("Data 1");
+        jPanel3.add(JFecha_Entrada, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 290, -1, -1));
+
+        JHora_Entrada.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        JHora_Entrada.setForeground(new java.awt.Color(153, 153, 153));
+        JHora_Entrada.setText("Data 2");
+        jPanel3.add(JHora_Entrada, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 320, -1, -1));
+
+        JPlaca_Vehiculo.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        JPlaca_Vehiculo.setForeground(new java.awt.Color(153, 153, 153));
+        JPlaca_Vehiculo.setText("Data 3");
+        jPanel3.add(JPlaca_Vehiculo, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 380, -1, -1));
 
         jLabel9.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
         jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/ticket.png"))); // NOI18N
         jPanel3.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, -1, -1));
-
-        jLabel13.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
-        jLabel13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/ticket-header.png"))); // NOI18N
-        jPanel3.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 80, -1, -1));
-
-        jLabel15.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
-        jLabel15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/ticket-header.png"))); // NOI18N
-        jPanel3.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 70, -1, -1));
-
-        jLabel16.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
-        jLabel16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/ticket-header.png"))); // NOI18N
-        jPanel3.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 70, -1, -1));
 
         jPanel2.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 30, 520, 770));
 
@@ -242,6 +313,26 @@ public class GenerarTicket extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_btnCancelarActionPerformed
 
+    private void txtEntradaKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEntradaKeyReleased
+        // TODO add your handling code here:
+        JFecha_Entrada.setText(txtEntrada.getText());
+    }//GEN-LAST:event_txtEntradaKeyReleased
+
+    private void txtHoraEntradaKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtHoraEntradaKeyReleased
+        // TODO add your handling code here:
+        JHora_Entrada.setText(txtHoraEntrada.getText());
+    }//GEN-LAST:event_txtHoraEntradaKeyReleased
+
+    private void BxPlacaVehItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_BxPlacaVehItemStateChanged
+        // TODO add your handling code here:}
+        JPlaca_Vehiculo.setText(BxPlacaVeh.getSelectedItem().toString());
+    }//GEN-LAST:event_BxPlacaVehItemStateChanged
+
+    private void BxUbiItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_BxUbiItemStateChanged
+        // TODO add your handling code here:
+        JUbicacion.setText(BxUbi.getSelectedItem().toString());
+    }//GEN-LAST:event_BxUbiItemStateChanged
+    
     /**
      * @param args the command line arguments
      */
@@ -284,6 +375,10 @@ public class GenerarTicket extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JComboBox<ComboBoxItemVeh> BxPlacaVeh;
     public javax.swing.JComboBox<ComboBoxItem> BxUbi;
+    private javax.swing.JLabel JFecha_Entrada;
+    private javax.swing.JLabel JHora_Entrada;
+    private javax.swing.JLabel JPlaca_Vehiculo;
+    private javax.swing.JLabel JUbicacion;
     public javax.swing.JButton btnCancelar;
     public javax.swing.JButton btnGrabar;
     private javax.swing.JCheckBox jCheckBox1;
@@ -296,12 +391,15 @@ public class GenerarTicket extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
-    private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel19;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel20;
     private javax.swing.JLabel jLabel21;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
